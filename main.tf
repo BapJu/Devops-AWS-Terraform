fcbe36272b0f75e782dbbd2c78e77590ef0dd76a
Baptiste JULIENNE <baptiste.julienne@gmail.com>
2025-03-20 15:10:02 +0100

    fix(api_gateway): correct authorization parameter in method resource
    The parameter `authorization_type` was incorrectly used in the `aws_api_gateway_method` resource. It has been updated to `authorization` to align with the correct AWS provider specification.

--- main.tf
+++ main.tf
@@ -62,10 +62,10 @@ resource "aws_api_gateway_resource" "resource" {
 
 # API Gateway Method
 resource "aws_api_gateway_method" "method" {
-  rest_api_id        = aws_api_gateway_rest_api.api.id
-  resource_id        = aws_api_gateway_resource.resource.id
-  http_method        = "GET"
-  authorization_type = "NONE"
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "GET"
+  authorization = "NONE"  # Changed from authorization_type to authorization
 }
 
 # API Gateway Integration
